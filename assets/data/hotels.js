b156b5b0d6eb26a65651b470743383d6b7e20d7f
Hieu1011 <20520994@gm.uit.edu.vn>
2023-04-09 13:58:17 +0700

    Update UI Navigate, Detail, Noti, Search, Suggest,...

--- assets/data/hotels.js
+++ assets/data/hotels.js
@@ -5,8 +5,11 @@ const hotels = [
     location: 'Green street,Central district',
     price: 120,
     image: require('../images/hotel1.jpg'),
+    slogan: 'Nơi tình cảm thăng hoa',
     details:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.7',
+    ratingvote: '(2.1m)',
   },
   {
     id: '2',
@@ -14,8 +17,11 @@ const hotels = [
     location: 'Yuki street',
     price: 70,
     image: require('../images/hotel2.jpg'),
+    slogan: 'Giao lưu ân ái thoải mái',
     details:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.6',
+    ratingvote: '(2m)',
   },
   {
     id: '3',
@@ -23,8 +29,11 @@ const hotels = [
     location: 'Almond street',
     price: 90,
     image: require('../images/hotel3.jpg'),
+    slogan: 'Độc - lạ - rẻ - trẻ - khỏe',
     details:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.0',
+    ratingvote: '(1.2m)',
   },
   {
     id: '4',
@@ -32,8 +41,47 @@ const hotels = [
     location: 'Main street',
     price: 100,
     image: require('../images/hotel4.jpg'),
+    slogan: 'Không chỉ là nơi để nghỉ',
     details:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.7',
+    ratingvote: '(2.4m)',
+  },
+  {
+    id: '5',
+    name: 'Green Hotel',
+    location: 'Main street',
+    price: 100,
+    image: require('../images/hotel4.jpg'),
+    slogan: 'Không chỉ là nơi để nghỉ',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.7',
+    ratingvote: '(2.4m)',
+  },
+  {
+    id: '6',
+    name: 'Green Hotel',
+    location: 'Main street',
+    price: 100,
+    image: require('../images/hotel4.jpg'),
+    slogan: 'Không chỉ là nơi để nghỉ',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.7',
+    ratingvote: '(2.4m)',
+  },
+  {
+    id: '7',
+    name: 'Green Hotel',
+    location: 'Main street',
+    price: 100,
+    image: require('../images/hotel4.jpg'),
+    slogan: 'Không chỉ là nơi để nghỉ',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit',
+    rating: '4.7',
+    ratingvote: '(2.4m)',
   },
 ];
 
